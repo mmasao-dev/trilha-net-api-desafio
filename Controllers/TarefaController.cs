bba180c303464236a919570a69158dcf5d55c5e2
mmasao-dev <mmasao@gmail.com>
2022-10-14 00:59:23 -0300

    Atualizar variaveis

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -90,6 +90,10 @@ namespace TrilhaApiDesafio.Controllers
 
             // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
             // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
             
             return Ok();
         }
